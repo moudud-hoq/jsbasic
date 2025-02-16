f49b5b3522327276129137c7aa65e28e38a473fc
moududulhaque <moududulhaque@gmail.com>
2025-02-16 19:16:32 +0600

    TYpe of check done

--- assets/js/js1.js
+++ assets/js/js1.js
@@ -6,3 +6,4 @@ let phone = "01700000000";
 let email = "tania98@gmial.com";
 
 let details = `Name: ${name}, Age: ${Age}, Address: ${address}, Country: ${country}, Phone: ${phone}, Email: ${email}`;
+
