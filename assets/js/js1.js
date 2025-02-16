78507cb4d1e64831498b2e849c6539d8329ca0b7
moududulhaque <moududulhaque@gmail.com>
2025-02-16 20:06:39 +0600

    Condition Done

--- assets/js/js1.js
+++ assets/js/js1.js
@@ -1,11 +1,8 @@
-a = 10;
-b = 20;
-y = 30;
+let a = 10;
+let b = 20;
 
-let c = a + b;
-let d = a - b;
-
-let e = b === a ? 'yes' : 'no';
-let f = b === d ? 'yes' : 'no';
-let g = b === c ? 'yes' : 'no';
-let h = c === y ? 'yes' : 'no';
+if (a == b) {
+    alert('this is od')
+} else {
+    alert('this is even')
+}
