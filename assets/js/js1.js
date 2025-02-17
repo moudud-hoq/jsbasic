fbbb9ce8315bd37dcb83a7de5c92d6e5a4823017
moududulhaque <moududulhaque@gmail.com>
2025-02-17 23:22:05 +0600

    Loop in Array

--- assets/js/js1.js
+++ assets/js/js1.js
@@ -2,8 +2,24 @@ let details = [
     userName = "John Doe",
     age = 25,
     mobile = 1234567890,
-    email = 'johndie@gmail.com'
+    email = 'johndie@gmail.com',
+    address = '123 Main St',
+    occupation = 'Software Developer',
+    company = 'Tech Solutions',
+    salary = 75000,
+    married = false,
+    nationality = 'American',
+    gender = 'Male',
+    hobbies = ['reading', 'coding', 'hiking'],
+    languages = ['English', 'Spanish'],
+    experience = 5,
+    degree = 'Bachelor of Science in Computer Science',
+    linkedin = 'https://www.linkedin.com/in/johndoe'
 ]
 
 console.log(details);
-console.log(details[3]);
+console.log(details[12.1])
+
+for (let i = 0; i < details.length; i++) {
+    console.log(details[i]);
+}
