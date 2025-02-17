8e4c5b2359f875112dc060f52904bcee964e9182
moududulhaque <moududulhaque@gmail.com>
2025-02-17 22:54:30 +0600

    Array Done in the rasel bhai video Shikhun.net

--- assets/js/js1.js
+++ assets/js/js1.js
@@ -1,3 +1,8 @@
-for (sum = 5; sum < 10; sum++) {
-    console.log(sum);
-}
+let details = [
+    userName = "John Doe",
+    age = 25,
+    mobile = 1234567890,
+    email = 'johndie@gmail.com'
+]
+
+console.log(details);
